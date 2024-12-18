ced186730ab8cb22ff6947dff95c7fbf6ad6cde7
SbWereWolf <ulfnew@gmail.com>
2024-12-19 01:09:44 +0500

    Add voucher creation

--- src/views/CreateVoucherView.vue
+++ src/views/CreateVoucherView.vue
@@ -1,18 +1,41 @@
 <template>
-  <BaseH1 size="large" :customStyles="{ paddingBottom: '60px', textAlign:'center' }"
+  <BaseH1
+    size="large"
+    :customStyles="{ paddingBottom: '60px', textAlign:'center' }"
   >
     Создать ваучер
   </BaseH1>
   <div class="centerCart">
-    <BaseP size="large">Количество картин</BaseP>
-    <BaseInput :customStyles="{ paddingBottom: '30px' }"/>
-    <BaseP size="large">Стиль</BaseP>
-    <BaseInput/>
-    <BaseP size="large">цена</BaseP>
-    <BaseInput :customStyles="{ paddingBottom: '30px' }"/>
-    <BaseP size="large">цена</BaseP>
-    <TextArea placeholder="Введите текст здесь..."/>
-    <BaseButton size="large" variant="primary60">Создать предзаказ</BaseButton>
+
+    <form @submit.prevent="placeVoucher">
+      <BaseP size="large">Количество картин</BaseP>
+      <BaseInput
+        type="number"
+        :customStyles="{ paddingBottom: '30px' }"
+        v-model="data.amount_pictures"
+      />
+      <BaseP size="large">Стиль</BaseP>
+      <BaseInput
+        placeholder='realism,modern,cubism,futurism,pop-art ...'
+        :customStyles="{ paddingBottom: '30px' }"
+        v-model="data.style"
+      />
+      <BaseP size="large">Цена</BaseP>
+      <BaseInput
+        type="number"
+
+        :customStyles="{ paddingBottom: '30px' }"
+        v-model="data.price"
+      />
+      <BaseP size="large">Пожелания</BaseP>
+      <TextArea
+        placeholder="Описание для картины здесь..."
+        v-model="data.description"
+      />
+      <BaseButton size="large" variant="primary60">
+        Создать ваучер
+      </BaseButton>
+    </form>
   </div>
 
 
@@ -24,4 +47,65 @@ import BaseP from '@/components/UI/P/BaseP.vue';
 import BaseButton from '@/components/UI/Button/BaseButton.vue';
 
 import TextArea from '@/components/UI/TextArea/TextArea.vue';
+import {reactive} from "vue";
+
+const data = reactive({
+  amount_pictures: 0,
+  price: 0,
+  style: '',
+  description: '',
+})
+
+const placeVoucher = async () => {
+  const token = window.localStorage.getItem('jwtToken');
+  if (!token) {
+    window.console.error('Токен отсутствует');
+    return;
+  }
+
+  const voucher = {
+    amount_pictures: data.amount_pictures,
+    price: data.price,
+    style: data.style,
+    description: data.description,
+    id: 0,
+    customer: '',
+    executor: '',
+    status: '',
+  };
+  window.console.log(voucher)
+
+  let response = {}
+  try {
+    response = await window.fetch(
+      import.meta.env.VITE_API_SERVER + '/Vouchers', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(voucher)
+
+      });
+  } catch (error) {
+    window.console.log(voucher);
+    window.console.error('Ошибка обработки запроса:', error);
+  }
+
+  if (!response.ok) {
+    const error = await response.json();
+    window.console.error('Ошибка создания ваучера:', error);
+  }
+
+  if (response.ok) {
+    const data = await response.json();
+    window.console.log('Ваучер добавлен успешно', data);
+    window.alert('Ваучер добавлен.')
+
+    data.amount_pictures = 0;
+    data.price = 0;
+    data.style = '';
+    data.description = '';
+  }
+};
 </script>
